3d6563037b8550da269c35671e9bead5b436753e
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-11-08 21:49:25 +0200

    Deploy

--- db/migrate/20120816192854_rename_case_id_to_case_detail_id.rb
+++
@@ -1,6 +0,0 @@
-class RenameCaseIdToCaseDetailId < ActiveRecord::Migration
-  def change
-#    rename_column :complainants, :case_id, :case_detail_id
-    rename_column :accuseds, :case_id, :case_detail_id
-  end
-end
