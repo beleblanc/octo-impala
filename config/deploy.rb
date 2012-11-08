438d742f5542c0bfd13478ca1ea4ee604b8b0024
Benoit LeBlanc <ben@out-there-websolutions.com>
2012-11-08 12:50:28 +0200

    Added whenever to capistrano recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 require "bundler/capistrano"
 
+
 load "config/recipes/base"
 load "config/recipes/nginx"
 load "config/recipes/unicorn"
@@ -22,5 +23,8 @@ set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+
+set :whenever_command, "bundle exec whenever"
+require 'whenever/capistrano'
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
